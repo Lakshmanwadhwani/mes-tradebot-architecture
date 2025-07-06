2888a047224f1c96d1ed5620e69fe9b52b3fbfbb
lakshman <lmarkal.wadhwani@gmail.com>
2025-07-05 19:14:44 -0500

    Delete pine_scripts/pipedream directory

--- pine_scripts/pipedream/tradovate_api/trade_executor_stub.js
+++
@@ -1,8 +0,0 @@
-
-// This is a non-functional placeholder to illustrate the architecture
-async function placeOrder({ symbol, action, price }) {
-  console.log(`Would place ${action} order for ${symbol} at ${price}`);
-  // In real system, would connect via Tradovate REST or WebSocket API
-}
-
-placeOrder({ symbol: 'MES', action: 'BUY', price: 5512.75 });
